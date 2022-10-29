16baf5a97780bfa345b826ca344822e0000ce6d2
gubersky <https://github.com/gubersky/Burger.git>
2022-10-30 02:58:38 +0300

    homework_3

--- homework/lesson_3/task_4.py
+++ homework/lesson_3/task_4.py
@@ -5,7 +5,7 @@ current_year = int(input_year)
 min_year = 1900
 max_year = 1000000
 
-if min_year <= current_year <= max_year:
+if min_year < current_year < max_year:
     if current_year % 4 == 0 and current_year % 100 != 0:
         print(current_year, ": is a leap year")
     elif current_year % 400 == 0:
