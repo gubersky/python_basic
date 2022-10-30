df4cfa8e2249ebca01d1015405300e0df8ea779d
gubersky <https://github.com/gubersky/Burger.git>
2022-10-30 03:31:35 +0300

    homework_3 update

--- homework/lesson_3/task_2.py
+++ homework/lesson_3/task_2.py
@@ -2,8 +2,15 @@ first_class = input("Input the number of students in first class")
 second_class = input("Input the number of students in second class")
 third_class = input("Input the number of students in third class")
 
-count_desks = int(first_class) + int(second_class) + int(third_class)
-count_remainder = count_desks % 2
-result = (count_desks + count_remainder) // 2
+count_first = int(first_class) % 2
+result_first = (int(first_class) + int(count_first)) // 2
 
-print("You need to buy:", result, "desks")
+count_second = int(second_class) % 2
+result_second = (int(second_class) + int(count_second)) // 2
+
+count_third = int(third_class) % 2
+result_third = (int(third_class) + int(count_third)) // 2
+
+overall = result_first + result_second + result_third
+
+print("You need to buy:", overall, "desks")
