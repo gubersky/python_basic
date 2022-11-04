bcfb4577c639e0ae40dedb28f2dda43bf4d66fa6
gubersky <https://github.com/gubersky/Burger.git>
2022-11-04 14:41:45 +0200

    homework_6 update

--- homework/lesson_5/task_1.py
+++ homework/lesson_5/task_1.py
@@ -1,10 +1,12 @@
-value = input("Input text: ").strip()
+value = input("Input to words using one a space: ")
 
-text = value.replace(" ", "")
-
-if text.isalpha():
-    text = value[::-1].title().split()
-    text[0], text[-1] = text[-1], text[0]
-    print(" ".join(text))
+if value.count(" ") == 1:
+    text = value.replace(" ", "")
+    if text.isalpha():
+        text = value[::-1].title().split()
+        text[0], text[-1] = text[-1], text[0]
+        print(" ".join(text))
+    else:
+        print("Wrong data! Please input the text")
 else:
-    print("Wrong data! Please input the text")
+    print("Wrong data! Please Input two words using one a space")
