2457efd52e3ab5a07b162fe719abb9b11c298853
gubersky <https://github.com/gubersky/Burger.git>
2022-11-12 01:12:24 +0200

    homework_8 update

--- homework/lesson_7/task_3.py
+++ homework/lesson_7/task_3.py
@@ -1,6 +1,8 @@
 first_list = [1, 2, 5, 7, 9, 99, 7, 200, 39, 2]
-second_list = [5, 42, 29, 345, 50, 33, 7, 2, 201, 9, 2, 132, 45, 23, 934]
+second_list = [5, 42, 29, 345, 50, 33, 7, 0, 201, 9, 2, 132, 45, 23, 934]
+
 count = 0
+
 for i in first_list:
     for k in second_list:
         if i == k:
