6d78fbb8cb3138ade98a0f233c46bde3a6e9b637
gubersky <https://github.com/gubersky/Burger.git>
2022-11-16 15:15:56 +0200

    homework_11 update

--- homework/lesson_9/work_11/task_1.py
+++ homework/lesson_9/work_11/task_1.py
@@ -1,4 +1,4 @@
 import random
 arr_set = set([random.randint(1, 15) for i in range(10)])
-print(arr_set)
+print(len(arr_set))
 
--- homework/lesson_9/work_11/task_2.py
+++ homework/lesson_9/work_11/task_2.py
@@ -1,4 +1,7 @@
-first_set = {33, 0, 103, 29, 10}
-second_set = {44, 33, 6, 10, 5}
+first_list = [33, 0, 103, 29, 10]
+second_list = [44, 33, 6, 10, 5]
 
-print(first_set & second_set)
+new_list = first_list + second_list
+
+print(set(new_list))
+
