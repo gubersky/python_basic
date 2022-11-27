2cd0425e28b7fa6cac40cdf61bae53b0eb87e0b0
gubersky <https://github.com/gubersky/Burger.git>
2022-11-27 21:43:35 +0200

    homework_13

---
+++ homework/lesson_11/task_1.py
@@ -0,0 +1,23 @@
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        for i in sorted(func(*args, **kwargs)):
+            if i % 2 == 0 and i // 3 * 3 == i:
+                print(f"Number: {i:<3} |{'multiple of 3':^18}| {'even number':<12}|")
+            if i % 2 == 0 and i // 3 * 3 != i:
+                print(f"Number: {i:<3} |{'not multiple of 3':^18}| {'even number':<12}|")
+            if i % 2 != 0 and i // 3 * 3 == i:
+                print(f"Number: {i:<3} |{'multiple of 3':^18}| {'odd number':<12}|")
+            if i % 2 != 0 and i // 3 * 3 != i:
+                print(f"Number: {i:<3} |{'not multiple of 3':^18}| {'odd number':<12}|")
+
+    return wrapper
+
+
+@decorator
+def set_number(n):
+    gen_set = set([i ** 2 for i in range(1, n + 1)])
+    return gen_set
+
+
+if __name__ == '__main__':
+    set_number(12)
---
+++ homework/lesson_11/task_2.py
@@ -0,0 +1,36 @@
+import string
+
+
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        while True:
+            print("Password must contains minimum 1 number, letter, special character and length at least 8 characters")
+            password = func(*args, **kwargs)
+            if password is None:
+                print("Wrong data: spaces and tabs are not accepted for the password!\n")
+            elif len([i for i in password if i.isdigit()]) == 0:
+                print("Password must contains minimum 1 number\n")
+            elif len([i for i in password if i.isalpha()]) == 0:
+                print("Password must contains minimum 1 letter\n")
+            elif not any([1 if i in string.punctuation else 0 for i in password]):
+                print("Password must contains minimum 1 special character\n")
+            elif len(password) < 8:
+                print("Password length must contains at least 8 characters\n")
+            else:
+                print("Password accepted:", password)
+                break
+
+    return wrapper
+
+
+@decorator
+def user_password():
+    password = input("Please enter the password: ")
+    if len(password) == 0 or password.count("\t") or password.count(" "):
+        return None
+    else:
+        return password
+
+
+if __name__ == '__main__':
+    user_password()
