26b415a987c67f11d858bc3576e31f697164931f
gubersky <https://github.com/gubersky/Burger.git>
2022-12-15 12:40:41 +0200

    update

--- homework/lesson_6/task_1.py
+++ homework/lesson_6/task_1.py
@@ -8,7 +8,8 @@ for i in range(1, 12 + 1):
     percent = 0.02 * remainder
     remainder = remainder - two_year_loan
     payment = percent + two_year_loan
-    print("Month = {:>2}: Monthly_payment = {:^10.2f}: Interest_per_month = {:>10.2f}".format(i, payment, percent))
+    print("Month = {:>2}: Monthly_payment = {:^10.2f}: "
+          "Interest_per_month = {:>10.2f}".format(i, payment, percent))
 
 remainder = money
 print("Loan for 5 years:")
@@ -18,7 +19,8 @@ for i in range(1, 60 + 1):
         percent = 0.02 * remainder
         remainder = remainder - five_year_loan
         payment = percent + five_year_loan
-        print("Month = {:>2}: Monthly_payment = {:^10.2f}: Interest_per_month = {:>10.2f}".format(i, payment, percent))
+        print("Month = {:>2}: Monthly_payment = {:^10.2f}: "
+              "Interest_per_month = {:>10.2f}".format(i, payment, percent))
     else:
         percent = 0.04 * remainder
         remainder = remainder - five_year_loan
